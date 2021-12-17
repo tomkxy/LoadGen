518dc95711549693c62ffe19bd9396098ae684a9
tkrebs <thomas.krebs@t-systems.com>
2021-12-17 14:06:32 +0100

    init

---
+++ src/test/scala/PublicationCreationSimulation.scala
@@ -0,0 +1,52 @@
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import java.util.UUID.randomUUID
+import scala.io.BufferedSource
+import concurrent.duration.DurationInt
+
+
+/**
+ * Load test for the rest service.
+ */
+class PublicationCreationSimulation extends io.gatling.core.scenario.Simulation
+  with SimulationConfig {
+
+  /**
+   * http configuration.
+   */
+  val httpProtocol = http.baseUrl(baseURL)
+
+  val pubId: String = ""
+
+  val feeder = Iterator.continually(Map("id" -> (randomUUID().toString)))
+
+  /**
+   * Scenario for simulation.
+   */
+  val scn = scenario("Simulation for publication creation").forever(
+      feed(feeder)
+      .exec(
+        http("create offer")
+          .put(offersEndpoint)
+          .header("Content-Type", "application/json")
+          .body(ElFileBody(templateFile))
+          .check(headerRegex("Location","""offeredResource/(\\*)""").saveAs(pubId))
+      )
+      .exec (
+        http("getoffer")
+          .get(offersEndpoint + "/" + pubId)
+      )
+    .pause(pause)
+  )
+
+
+  /**
+   * Sets the scenario.
+   */
+  setUp(scn.inject(atOnceUsers(user_count)))
+    .maxDuration(duration.minutes)
+    .protocols(httpProtocol)
+    .assertions(global.successfulRequests.percent.is(percentSuccess)) //Check test result
+}
+
